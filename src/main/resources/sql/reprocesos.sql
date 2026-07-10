9e1afbe1c3345551db2587502d7ef8cf1699c5be
Mirko Gutierrez <mirkogutierezaravena@gmail.com>
2026-07-10 15:50:46 -0400

    agraega campo LIQVISIBLEWEB

--- src/main/resources/sql/reprocesos.sql
+++ src/main/resources/sql/reprocesos.sql
@@ -1,4 +1,4 @@
-select r.IDENT ,r.ANOREMUN , r.MESREMUN ,r.DESCRIPCIONPROCESO , r.NROLIQ   from REPROCESOS r
+select r.IDENT ,r.ANOREMUN , r.MESREMUN ,r.DESCRIPCIONPROCESO , r.NROLIQ , r.LIQVISIBLEWEB  from REPROCESOS r
 where  r.IDENT = :ident  and r.ANOREMUN = :anio and 
 exists  (select * from reliquidacion z where z.ident=r.IDENT  and r.NROLIQ =z.linliq  and z.rut= :rut )
 order by r.ANOREMUN desc
